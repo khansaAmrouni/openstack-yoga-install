48d478c97604709635b7795f55f554654a48b8bf
khansaAmrouni <khansa.amrouni@gmail.com>
2022-07-22 12:50:56 +0100

    update keystone url

--- openstack-install.sh
+++ openstack-install.sh
@@ -209,7 +209,7 @@ export OS_USER_DOMAIN_NAME=Default
 export OS_PROJECT_NAME=admin
 export OS_USERNAME=admin
 export OS_PASSWORD="${ADMIN_PASS}"
-export OS_AUTH_URL=http://controller:5000/identity/v3
+export OS_AUTH_URL=http://controller/identity/v3
 export OS_IDENTITY_API_VERSION=3
 export OS_IMAGE_API_VERSION=2
 
@@ -219,7 +219,7 @@ export OS_USER_DOMAIN_NAME=Default
 export OS_PROJECT_NAME=admin
 export OS_USERNAME=admin
 export OS_PASSWORD="${ADMIN_PASS}"
-export OS_AUTH_URL=http://controller:5000/identity/v3
+export OS_AUTH_URL=http://controller/identity/v3
 export OS_IDENTITY_API_VERSION=3
 export OS_IMAGE_API_VERSION=2"
 
@@ -234,7 +234,7 @@ export OS_USER_DOMAIN_NAME=Default
 export OS_PROJECT_NAME=myproject
 export OS_USERNAME=myuser
 export OS_PASSWORD="${MYUSER_PASS}"
-export OS_AUTH_URL=http://controller:5000/v3
+export OS_AUTH_URL=http://controller/identity/v3
 export OS_IDENTITY_API_VERSION=3
 export OS_IMAGE_API_VERSION=2"
 
